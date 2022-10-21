b99443a46798f8f230d92256854bd8df27e28d8c
ubaidmalik0303 <ubaidmalik0303@gmail.com>
2022-10-21 11:42:24 +0500

    Initial commit from gatsby

---
+++ gatsby-config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  siteMetadata: {
+    title: `Wall To Wall Carpets `,
+    siteUrl: `https://www.yourdomain.tld`
+  },
+  plugins: ["gatsby-plugin-sitemap", {
+    resolve: 'gatsby-plugin-manifest',
+    options: {
+      "icon": "src/images/icon.png"
+    }
+  }]
+};
